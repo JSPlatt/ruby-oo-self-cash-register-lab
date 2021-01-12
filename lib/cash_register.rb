0fa2a7928be05a6d4e6cba77d94933481aeddccb
Jeffrey Platt <JSPlatt@gmail.com>
2021-01-12 15:27:33 -0500

    commit tree 9842533fd93bde7131435f54fcf19ad58f1e0b02

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+    attr_accessor :total, :discount, :items, :last_item
 
+    def initialize(discount=nil, total=0, items =[])
+        @discount = discount
+        @total = total
+        @items = items
+    end
+
+    def add_item(name, price, quantity)
+        self.last_item = price * quantity
+        self.total += (price * quantity)
+        quantity.times {@items.push(name)}
+    end
+end
